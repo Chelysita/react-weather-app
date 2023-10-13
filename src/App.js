2bc305fc9321fc54b56847605cb8670b85cd3754
Chelysita <ara.medinago@gmail.com>
2023-10-13 15:04:33 -0400

    Adding the repository to Netlify

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Let's do this</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Chelysita/react-weather-app.git"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          open-source code
         </a>
       </header>
     </div>
