64c8ec32ac2536431fd3cd82cf1229bd64173d0f
Chelysita <ara.medinago@gmail.com>
2023-11-01 12:05:19 -0400

    Form and style added

--- src/App.js
+++ src/App.js
@@ -5,21 +5,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <footer className="App-header">
-        <Weather />
-        <p>Let's do this</p>
-        <a
-          className="App-link"
-          href="https://github.com/Chelysita/react-weather-app.git"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-source code
-        </a>
-        <button type="button" class="btn">
-          Button
-        </button>
-      </footer>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -5,8 +5,89 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div>
-      <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
-        Example element with utilities
+      <div className="weatherApp container-fluid">
+        <div className="container-fluid text-center mt-5 mb-3" mclass="mx-auto">
+          <form id="form">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  class="form-control"
+                  placeholder="Enter a city"
+                  id="city-NewInput"
+                  autocomplete="off"
+                  autofocus="on"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  className="form-control btn btn-primary"
+                  value="Search"
+                />
+              </div>
+            </div>
+          </form>
+          <br />
+          <h1>Boston</h1>
+          <h4>Last updated: Monday 13:35</h4>
+        </div>
+
+        <div className="container-fluid text-center">
+          <div className="row container-fluid">
+            <div className="col align-self-center container-fluid">
+              <img src="images/drop3.png" width="100%" />
+              <div className="humidityIcon">
+                <span id="humidity"></span>%
+              </div>
+            </div>
+            <div className="col container-fluid">
+              <div
+                className="forecast p-2 container-fluid"
+                id="weatherDescription"
+              ></div>
+              <img className="forecastIcon" width="50%" id="icon" />
+
+              <div className="forecast p-2 container-fluid">
+                <span id="temperature"></span>
+                <span className="unitStyle">
+                  <a href="#" class="under unitColor" id="celsiusLink">
+                    °F{" "}
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="col align-self-center">
+              <img
+                className="forecastIcon"
+                src="images/wind.png"
+                width="100%"
+              />
+              <div className="windSpeed">
+                <span id="wind"></span> mph
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container text-center">
+          <div className="row align-items-center">
+            <div className="col">One of three columns</div>
+            <div className="col">One of three columns</div>
+            <div className="col">One of three columns</div>
+            <div className="col">One of four columns</div>
+            <div className="col">One of five columns</div>
+          </div>
+        </div>
+        <footer className="App-footer text-center">
+          <a
+            className="App-link"
+            href="https://github.com/Chelysita/react-weather-app.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-source code
+          </a>
+        </footer>
       </div>
     </div>
   );
